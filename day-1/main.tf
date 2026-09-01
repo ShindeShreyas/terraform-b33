da1ca89c846dacd5cd8fb206d92e66e8de6ef735
shindeshreas <shindeshreyas0211@gmail.com>
2026-09-01 15:51:06 +0530

    terra

--- day-1/main.tf
+++ day-1/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
     ami = "ami-0086ee55a149bd32e"
     instance_type = "t3.micro"
     key_name = "ubuntu-1"
-    vpc_security_group_ids = ["ami-0086ee55a149bd32e"]
+    vpc_security_group_ids = ["sg-09e1d1fffa15b8601"]
     tags = {
         Name = "my_ec2"
     }
