c1219bb21429938a6b697e51ee9116ed22db5a62
shindeshreas <shindeshreyas0211@gmail.com>
2026-09-03 15:40:35 +0530

    day 2

--- day-2-vpc/provider.tf
+++ day-2-vpc/provider.tf
@@ -1,4 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.62.0"
+    }
+  }
+}
+
 provider "aws" {
-    region = "ap-south-1"
-    profile = "default"
+  region  = "ap-south-1"
+  profile = "default"
 }
