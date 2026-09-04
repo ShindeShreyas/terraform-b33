9bafe70c1d013ae5727002f32e69e0a015d61c51
shindeshreas <shindeshreyas0211@gmail.com>
2026-09-04 16:07:53 +0530

    fix: remove nginx enable command from user data script

--- day-3-lb-asg/user_data.sh
+++ day-3-lb-asg/user_data.sh
@@ -2,7 +2,6 @@
 apt update -y
 apt install -y nginx 
 systemctl start nginx 
-systemctl enable nginx 
 rm -rf /usr/share/nginx/html/*
 echo "<h1> Hello From $HOSTNAME </h1>" > /usr/share/nginx/html/index.html
 systemctl restart nginx
