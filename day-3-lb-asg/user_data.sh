7a0c5418e9352a067ab2b82e71448607265b104f
shindeshreas <shindeshreyas0211@gmail.com>
2026-09-04 15:26:20 +0530

    feat: add initial Terraform configuration for AWS infrastructure setup

---
+++ day-3-lb-asg/user_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt update -y
+apt install -y nginx 
+systemctl start nginx 
+systemctl enable nginx 
+rm -rf /usr/share/nginx/html/*
+echo "<h1> Hello From $HOSTNAME </h1>" > /usr/share/nginx/html/index.html
+systemctl restart nginx
