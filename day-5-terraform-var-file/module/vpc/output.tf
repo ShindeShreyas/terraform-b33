8fbd0ee2aca74a82dabc3a6645d4a0e94e15aec7
shindeshreas <shindeshreyas0211@gmail.com>
2026-09-07 16:13:42 +0530

    fix: correct subnet ID references in VPC output

--- day-5-terraform-var-file/module/vpc/output.tf
+++ day-5-terraform-var-file/module/vpc/output.tf
@@ -1,11 +1,15 @@
+output "vpc_id" {
+    value = aws_vpc.my_vpc.id 
+}
+
 output "public_subnet_id" {
-  value = aws_subnet.public.id
+    value = aws_subnet.public_subnet.id 
 }
 
 output "private_subnet_id" {
-  value = aws_subnet.private.id
+    value = aws_subnet.private_subnet.id 
 }
 
 output "sg_id" {
-  value = aws_security_group.sg.id
+    value = aws_security_group.sg.id 
 }
