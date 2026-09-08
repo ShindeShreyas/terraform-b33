63f780c8d8f3589b073cf7bfff9400d1b412c157
shindeshreas <shindeshreyas0211@gmail.com>
2026-09-08 15:03:59 +0530

    fix: add subnet ID for public instance in main.tf

--- day-6-state-management/main.tf
+++ day-6-state-management/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "public_instance" {
     ami = "ami-01a00762f46d584a1"
     instance_type = "t3.micro"
     key_name = "ubuntu"
+    subnet_id = "subnet-06567d46491f653b9"
     count = 2
     tags = {
         Name = "public_instance"
