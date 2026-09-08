2e3121b3d648684c39bbfeddbededaea337f7380
shindeshreas <shindeshreyas0211@gmail.com>
2026-09-08 14:35:47 +0530

    fix: ensure correct subnet ID for public instance in main.tf

--- day-6-state-management/main.tf
+++ day-6-state-management/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "public_instance" {
     ami = "ami-01a00762f46d584a1"
     instance_type = "t3.micro"
     key_name = "ubuntu"
+    subnet_id = "subnet-0ea6acab57db6ad9b"
     count = 2
     tags = {
         Name = "public_instance"
