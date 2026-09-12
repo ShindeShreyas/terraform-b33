4ecd3a1959b82765ab403fff7eeae84a08c279c4
shindeshreas <shindeshreyas0211@gmail.com>
2026-09-12 10:46:05 +0530

    feat: add EC2 and VPC modules with variables for configuration in main.tf, provider.tf, and var.tf

--- day-6-state-management/main.tf
+++ day-6-state-management/main.tf
@@ -8,3 +8,4 @@ resource "aws_instance" "public_instance" {
         Name = "public_instance"
     }
 }
+
---
+++ day-8-loops/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "ec2" {
+    for_each = tomap({
+        server-1 = "t3.micro"
+        server-2 = "t3.small"
+        server-3 = "c7i-flex.large"
+    })
+    ami = "ami-02167eae61967e403"
+    instance_type = each.value
+    key_name = "abhi"
+    tags = {
+        Name = each.key
+    } 
+}
